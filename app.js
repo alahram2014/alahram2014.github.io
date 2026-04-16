1ed553017af24e41e25dd54b75951e0f12e62888
alahram2014 <alahram.group.co@gmail.com>
2026-04-16 13:09:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ async function loadData() {
     const res = await fetch(API_URL);
     const data = await res.json();
 
-    DATA.products = data.products || [];
+    DATA.products = typeof data.products === "string"   ? JSON.parse(data.products)   : (data.products || []);
 
     const map = {};
     DATA.products.forEach(p => {
