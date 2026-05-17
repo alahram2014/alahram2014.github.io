90e71b29fffc817cafc021a0b6e8416250f32866
alahram2014 <alahram.group.co@gmail.com>
2026-05-17 21:07:34 +0300

    Delete src directory

--- src/ops/runtime/opsRuntime.js
+++
@@ -1,9 +0,0 @@
-import { hydrateOpsRuntime } from '../hydration/runtimeHydration.js';
-import { shouldBlockCheckout } from '../guards/checkoutGuard.js';
-
-export function createOpsRuntime() {
-  return {
-    hydrate: hydrateOpsRuntime,
-    shouldBlockCheckout,
-  };
-}
